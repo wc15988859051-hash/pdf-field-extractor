a68100a52bcb9d639c8541a9de31ecbb1cc36c3c
C_dot <775604657216944-C_dot@noreply.coze.cn>
2026-03-02 16:47:27 +0800

    Restored to '6671a60a011c4af99cac557f416019ebd40a9e34'
    Coze-Restored-To: '6671a60a011c4af99cac557f416019ebd40a9e34'
    Coze-Commit-Type: restore

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,8 @@ import type { NextConfig } from 'next';
 import path from 'path';
 
 const nextConfig: NextConfig = {
-  outputFileTracing: true,
-  output: 'standalone',
+  // outputFileTracingRoot: path.resolve(__dirname, '../../'),
+  /* config options here */
   allowedDevOrigins: ['*.dev.coze.site'],
   images: {
     remotePatterns: [
@@ -13,12 +13,6 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
     ],
-  },
-  // 增加 body 大小限制
-  experimental: {
-    serverActions: {
-      bodySizeLimit: '10mb',
-    },
   },
 };
 
